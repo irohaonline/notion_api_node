f69c6c938a16f50d643e0fc51c4e8c6d249291a1
irohaonline <onlineiroha@gmail.com>
2021-07-17 20:01:13 +0900

    feat: docerlize fix: listening port

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const getPage = require("./notion/client/getPage");
 const app = express();
-const PORT = 3000;
+
+// Constants
+const PORT = 8080;
+const HOST = "0.0.0.0";
 
 app.use(express.json());
 app.listen;
-
-app.listen(PORT, () => console.log(`Port ${PORT} listening...`));
 
 app.get("/", async (req, res) => {
   const fetchedPageContent = await getPage(process.env.NOTION_PAGE_ID);
   res.status(200).send(fetchedPageContent);
 });
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
